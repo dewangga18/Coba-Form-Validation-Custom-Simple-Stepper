8eaf9aa1c0f93b699acf122df964c07c65e3ebc9
Aaron Evanjulio <aaronevanjulio18@gmail.com>
2024-07-29 16:00:05 +0700

    Initial Commit

---
+++ SimpleForm/SimpleFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFormApp.swift
+//  SimpleForm
+//
+//  Created by admin on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
